ab1b8520b1eadcac6d64c53ac2ead0576c441ae2
= <=>
2022-03-21 10:39:37 -0400

    Added storage functionality to limit API calls

--- JamfPro/Public/func_Get-Groups.ps1
+++ JamfPro/Public/func_Get-Groups.ps1
@@ -5,8 +5,19 @@ function Get-Groups
         [Parameter(Position = 1, Mandatory = $false)][switch]$Computer,
         [Parameter(Position = 2, Mandatory = $false)][switch]$User,
         [Parameter(Position = 3, Mandatory = $false)][switch]$Mobile,
-        [Parameter(Position = 4, Mandatory = $false)][pscredential]$Credential
+        [Parameter(Position = 4, Mandatory = $false)][pscredential]$Credential,
+        [Parameter(Position = 5, Mandatory = $false)][switch]$refresh
     )
+
+    if ($Computer)
+    {
+        $storedData = $global:ComputerGroups | Where-Object {$_.Server -eq $Server} | Select-Object "data"
+    }
+
+    if ((-not $null -eq $storedData) -and (-not $refresh))
+    {
+        return $storedData.data
+    }
 
     if ($null -eq $Credential)
     {
@@ -34,5 +45,8 @@ function Get-Groups
     Write-Host "Attempting to call on $URI"
     $response = Invoke-RestMethod $URI -Method Get -Authentication Basic -Credential $Credential -ContentType 'application/xml;charset=UTF-8'
 
+    $global:ComputerGroups = @{"Server" = $Server
+    "data" = $response.computer_groups
+    }
     return $response.computer_groups
 }
