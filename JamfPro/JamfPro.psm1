5e6226e088fa2081133cc6c9a8ce314b066cadd7
= <=>
2022-03-21 16:30:36 -0400

    Added ability to pull policy data

--- JamfPro/JamfPro.psm1
+++ JamfPro/JamfPro.psm1
@@ -9,6 +9,10 @@ Get-ChildItem "$(Split-Path $script:MyInvocation.MyCommand.Path)\Public\Token\*"
     Export-ModuleMember -Function ($_.BaseName -Split "_")[1] 
     }
 
-    Get-ChildItem "$(Split-Path $script:MyInvocation.MyCommand.Path)\Public\Group\*" -Filter 'func_*.ps1' -Recurse | ForEach-Object { 
-        Export-ModuleMember -Function ($_.BaseName -Split "_")[1] 
-        }
+Get-ChildItem "$(Split-Path $script:MyInvocation.MyCommand.Path)\Public\Group\*" -Filter 'func_*.ps1' -Recurse | ForEach-Object { 
+    Export-ModuleMember -Function ($_.BaseName -Split "_")[1] 
+    }
+
+Get-ChildItem "$(Split-Path $script:MyInvocation.MyCommand.Path)\Public\Policy\*" -Filter 'func_*.ps1' -Recurse | ForEach-Object { 
+    Export-ModuleMember -Function ($_.BaseName -Split "_")[1] 
+    }
---
+++ JamfPro/Public/Policy/func_Get-Policy.ps1
@@ -0,0 +1,51 @@
+function Get-Policy
+{
+    Param(
+        [Parameter(Position = 4, Mandatory = $true)][String]$Server,
+        [Parameter(Position = 2, Mandatory = $false)][pscredential]$Credential,
+        [Parameter(Position = 3, Mandatory = $false)][String]$Token,
+        [Parameter(Position = 0, Mandatory = $false)][String]$Id,
+        [Parameter(Position = 0, Mandatory = $false)][String]$Name,
+        [Parameter(Position = 0, Mandatory = $false)][String]$Subset
+    )
+
+    if ($null -eq $Id -and $null -eq $Name)
+    {
+        throw "An `"-Id`" or `"-Name`" must be provided."
+    }
+
+    if (($null -eq $Credential) -and ($null -eq $Token))
+    {
+        # Prompt for credentials if none were provided
+        $Credential = Get-Credential
+    }
+
+    if (-not $null -eq $Id)
+    {
+        $URI = "$Server/JSSResource/policies/id/$Id"
+    }
+    elseif (-not $null -eq $Name) {
+        $URI = "$Server/JSSResource/policies/name/$Name"
+    }
+
+    if (-not $null -eq $Subset)
+    {
+        $URI += "/subset/$Subset"
+    }
+
+    if ($null -eq $Token)
+    {
+        $headers = @{"Accept" = "application/json"}
+      
+        $response = Invoke-RestMethod $URI -Method Get -Headers $headers -Credential $Credential -Authentication Basic
+        return $response.policy
+    }
+    else
+    {
+        $headers = @{"Accept" = "application/json"
+            "Authorization" = "Bearer $Token"
+        }
+        $response = Invoke-RestMethod $URI -Method Get -Headers $headers
+        return $response.policy
+    }
+}
