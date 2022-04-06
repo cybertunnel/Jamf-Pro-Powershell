6acddc3de73c0c5c38f42b4db23bef023f768b04
= <=>
2022-04-06 10:31:34 -0400

    Added additional functions to profiles

--- JamfPro/Public/Configuration Profile/func_Get-ConfigurationProfile.ps1
+++ JamfPro/Public/Configuration Profile/func_Get-ConfigurationProfile.ps1
@@ -1,3 +1,6 @@
+############################
+# API Type: Legacy / Classic
+# --------------------------
 # Documentation Reference:
 # - Computer by ID: https://developer.jamf.com/jamf-pro/reference/findosxconfigurationprofilesbyid
 # - Computer by Name: https://developer.jamf.com/jamf-pro/reference/findosxconfigurationprofilesbyname
@@ -7,52 +10,69 @@
 function Get-ConfigurationProfile
 {
     Param(
-        [Parameter(Position = 0, Mandatory = $true)][String]$Server,
-        [Parameter(Position = 2, Mandatory = $false)][pscredential]$Credential,
-        [Parameter(Position = 2, Mandatory = $false)][String]$Token,
-        [Parameter(Position = 2, Mandatory = $false)][Int]$Id,
-        [Parameter(Position = 2, Mandatory = $false)][String]$Name,
-        [Parameter(Position = 2, Mandatory = $false)][Switch]$Computer
+        [CmdletBinding(DefaultParameterSetName='single')]
+        # Jamf Pro server
+        [Parameter(Position = 0,
+            Mandatory)]
+        [ValidateScript({-not [String]::IsNullOrEmpty($_)})]
+        [String]$Server,
+
+        # Token as string
+        [Parameter(Position = 1,
+            Mandatory)]
+        [ValidateScript({-not [String]::IsNullOrEmpty($_)})]
+        [String]$Token,
+        
+        [Parameter(Position = 3,
+            ParameterSetName='single')]
+        [Int]$Id,
+
+        [Parameter(Position = 3,
+            ParameterSetName='single')]
+        [String]$Name,
+
+        [Parameter(Position = 2)]
+        [Switch]$Computer,
+
+        [Parameter(Position = 2,
+            ParameterSetName='all')]
+        [Switch]$All
     )
+
+    $URI_PATH = "JSSResource/osxconfigurationprofiles"
+    $URI = "$Server/$URI_PATH"
 
     if (-not $Computer)
     {
         throw "An extension attribute type flag must be provided. Supported extension attributes: `"-Computer`"."
     }
 
-    if (($null -eq $Credential) -and ($null -eq $Token))
+    if (-not $All)
     {
-        # Prompt for credentials if none were provided
-        $Credential = Get-Credential
-    }
 
-    if (-not $null -eq $Id)
-    {
-        $URI = "$Server/JSSResource/osxconfigurationprofiles/id/$Id"
-    }
-    elseif (-not $null -eq $Name)
-    {
-        $URI = "$Server/JSSResource/osxconfigurationprofiles/name/$Name"
-    }
-    else
-    {
-        throw "Either a `"-Id`" or `"-Name`" must be provided."
-    }
-
-    if ($null -eq $Token)
-    {
-        $headers = @{"Accept" = "application/json"}
-        $response = Invoke-RestMethod $URI -Method Get -Headers $headers -Credential $Credential -Authentication Basic
-    }
-    else
-    {
-        $headers = @{"Accept" = "application/json"
-            "Authorization" = "Bearer $Token"
+        if (-not $null -eq $Id)
+        {
+            $URI += "/id/$Id"
         }
-        $response = Invoke-RestMethod $URI -Method Get -Headers $headers
+        elseif (-not $null -eq $Name)
+        {
+            $URI += "/name/$Name"
+        }
+        else
+        {
+            throw "Either a `"-Id`" or `"-Name`" must be provided."
+        }
     }
+    $headers = @{"Accept" = "application/json"
+            "Authorization" = "Bearer $Token"}
+    $response = Invoke-RestMethod $URI -Method Get -Headers $headers
 
-    return $response.os_x_configuration_profile
-
-    return
+    if ($All)
+    {
+        return $response.os_x_configuration_profiles
+    }
+    else
+    {
+        return $response.os_x_configuration_profile
+    }
 }
