78c42e4005fb2852982bc30c7e00e499727bce4d
= <=>
2022-05-18 18:07:19 -0400

    XML processing not fixed

--- JamfPro/Public/func_ConvertTo-JamfXML.ps1
+++ JamfPro/Public/func_ConvertTo-JamfXML.ps1
@@ -9,24 +9,35 @@ function ConvertTo-JamfXML
 
     $newObjectType = $xmlObj.AppendChild($xmlObj.CreateElement($ParentElementName))
 
+    # EX: General / Scope
     $parentElements = $JamfObject | Get-Member | Where-Object {$_.MemberType -eq 'NoteProperty'} | Select-Object -ExpandProperty Name
 
     foreach ($parentElement in $parentElements)
     {
-        Write-Host "Processing Element $parentElement"
+        #Write-Host "Processing Element $parentElement"
         $newParentElement = $newObjectType.AppendChild($xmlObj.CreateElement($parentElement))
 
         # Check if Custom Object or Array with contents
         if ($JamfObject.$parentElement -is [PSCustomObject] -or ($JamfObject.$parentElement -is [Array] -and $JamfObject.$parentElement.count -gt 0))
         {
-            Write-Host "Parent has children!"
+            #Write-Host "Parent has children!"
 
             $childrenElements = $JamfObject.$parentElement | Get-Member | Where-Object {$_.MemberType -eq 'NoteProperty'} | Select-Object -ExpandProperty Name
 
             foreach ($childElement in $childrenElements)
             {
-                Write-Host "Processing Child Element $childElement"
+                #Write-Host "Processing Child Element $childElement"
                 $newChildElement = $newParentElement.AppendChild($xmlObj.CreateElement($childElement))
+
+                if ($parentElement -eq 'scope' -and -not $childElement -eq "limitations" -and -not $childElement -eq "exclusions" -and -not $childElement -eq "all_computers")
+                {
+                    $newChildElement = $newChildElement.AppendChild($xmlObj.CreateElement($childElement.Substring(0, $childElement.length -1)))
+                    Write-Host "Child element: $childElement"
+                }
+                elseif ($parentElement -eq 'scope' -and $childElement -eq "buildings")
+                {
+                    Write-Host "False object of $childElement"
+                }
 
                 # Chick if Custom Object or Array with content
                 if ($JamfObject.$parentElement.$childElement -is [PSCustomObject] -or ($JamfObject.$parentElement.$childElement -is [Array] -and $JamfObject.$parentElement.$childElement.count -gt 0))
@@ -35,17 +46,23 @@ function ConvertTo-JamfXML
 
                     foreach ($childChildElement in $childrenChildrenElements)
                     {
-                        Write-Host "Processing Child element of Child element $childChildElement"
+                        if ($parentElement -eq 'scope')
+                        {
+                            Write-Host "Processing child child element of $childChildElement"
+                        }
                         $newChildChildElement = $newChildElement.AppendChild($xmlObj.CreateElement($childChildElement))
 
                         # Chick if Custom Object or Array with content
                         if ($JamfObject.$parentElement.$childElement.$childChildElement -is [PSCustomObject] -or ($JamfObject.$parentElement.$childElement.$childChildElement -is [Array] -and $JamfObject.$parentElement.$childElement.$childChildElement.count -gt 0))
                         {
-                            Write-Host "Max depth reached for $JamfObject.$parentElement.$childElement.$childChildElement"
                             $newChildChildElement.AppendChild($xmlObj.CreateTextNode($JamfObject.$parentElement.$childElement.$childChildElement))
                         }
                         else
                         {
+                            if ($parentElement -eq 'scope' -and -not $childElement -eq 'limitations' -and -not $childElement -eq 'exclusions')
+                            {
+                                Write-Host "YEEEHAW"
+                            }
                             $newChildChildElement.AppendChild($xmlObj.CreateTextNode($JamfObject.$parentElement.$childElement.$childChildElement))
                         }
                     }
