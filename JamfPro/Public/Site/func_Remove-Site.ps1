937d6f7eed65a2af70cea2131138c813993651b3
= <=>
2022-06-22 16:09:38 -0400

    made function a bit more streamlined

--- JamfPro/Public/Site/func_Remove-Site.ps1
+++ JamfPro/Public/Site/func_Remove-Site.ps1
@@ -21,26 +21,11 @@ function Remove-Site
         [Int]$Id
     )
 
-    if (($null -eq $Credential) -and ($null -eq $Token))
-    {
-        # Prompt for credentials if none were provided
-        $Credential = Get-Credential
-    }
-
     $URI = "$Server/JSSResource/sites/id/$Id"
-
-    if (-not $null -eq $token)
-    {
-        $headers = @{"Authorization" = "Bearer $token"
-            "Accept" = "application/json"    
-        }
-        $response = Invoke-RestMethod $URI -Method Delete -Headers $headers
-        return $response
-
+    
+    $headers = @{"Authorization" = "Bearer $token"
+        "Accept" = "application/json"    
     }
-    else {
-        $headers = @{"Accept" = "application/json"}
-        $response = Invoke-RestMethod $URI -Method Delete -Authentication Basic -Credential $Credential -Headers $headers
-        return $response
-    }
+    $response = Invoke-RestMethod $URI -Method Delete -Headers $headers
+    return $response
 }
